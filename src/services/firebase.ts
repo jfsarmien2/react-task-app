efa0fc42e02934713af1bc69d15f50e4072d530b
John <john.phcciholding@gmail.com>
2024-02-19 17:37:25 +0800

    update firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -8,12 +8,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyDp8HFvGJ1hLiM5hllTqHFrVVa7DdmNxs4",
-  // authDomain: "react-typescript-app-4abcc.firebaseapp.com",
-  // projectId: "react-typescript-app-4abcc",
-  // storageBucket: "react-typescript-app-4abcc.appspot.com",
-  // messagingSenderId: "1015426707070",
-  // appId: "1:1015426707070:web:835fca784f76f5cb7fb22f",
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
